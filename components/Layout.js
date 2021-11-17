68916e82c3b1a596a0b3c5da2a57b59452fcc767
wei30172 <claire610323@gmail.com>
2021-11-16 17:43:29 -0800

    initial

--- components/Layout.js
+++ components/Layout.js
@@ -7,10 +7,10 @@ export default function Layout({ children }) {
         <Link href="/">
           <a>
             <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
+              <span>Welcome To My</span>
+              <span>Kitchen Cookbook</span>
             </h1>
-            <h2>Spread The Joy</h2>
+            <h2>Spread The Happiness</h2>
           </a>
         </Link>
       </header>
@@ -20,7 +20,7 @@ export default function Layout({ children }) {
       </div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>Copyright 2021 Cook For Love :)</p>
       </footer>
     </div>
   )
